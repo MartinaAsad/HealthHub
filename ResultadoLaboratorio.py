ae8cde978b7e1ee8e3b62a7fe7853c43fd8f48c3
MartinaAsad <lasupermartu15@gmail.com>
2023-09-28 19:42:35 -0300

    cree varias clases nuevas

---
+++ ResultadoLaboratorio.py
@@ -0,0 +1,7 @@
+class ResultadoLaboratorio:
+    def __init__(self, ID, FechaResultado, Resultado, Laboratorio, Paciente):
+        self.ID = ID
+        self.FechaResultado = FechaResultado
+        self.Resultado = Resultado
+        self.Laboratorio = Laboratorio
+        self.Paciente = Paciente
